e58b3b62dc8a8af78a31dcf9c3089428c64dc7f6
Ralston <rally619@gmail.com>
2024-05-20 17:57:55 -0400

    initial commit

---
+++ BusinessPageApp/BusinessPageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessPageAppApp.swift
+//  BusinessPageApp
+//
+//  Created by Ralston Goes on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BusinessPageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusinessPageApp/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  BusinessPageApp
+//
+//  Created by Ralston Goes on 5/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
